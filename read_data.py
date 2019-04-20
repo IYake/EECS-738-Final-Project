b9c6f2fab76cb835bd39d4cd14c2d64d654d7966
Jules Garrett <jrgarrett3222@gmail.com>
2019-04-20 12:27:09 -0500

    add file to read data

---
+++ read_data.py
@@ -0,0 +1,27 @@
+import numpy as np
+import os
+from PIL import Image
+import cv2
+from tqdm import tqdm
+import pprint as pp
+import scipy
+import csv
+train_path = "/Users/julesgarrett/Downloads/tiny-imagenet-200/train"
+label_path = "/Users/julesgarrett/Downloads/tiny-imagenet-200"
+IMG_SIZE = 64
+
+
+train_data =[]
+for file in tqdm(os.listdir(train_path)):
+    for pic in os.listdir(train_path+"/"+file+"/images"):
+        img = cv2.imread(train_path+"/"+file+"/images/"+pic)
+        train_data.append(np.array(img))
+np.save('tiny-imagenet-train.npy', train_data)
+
+
+# labels = read.csv(label_path+"/"+"words.txt")
+labels = []
+with open(label_path+"/"+'words.txt') as f:
+    reader = csv.reader(f, delimiter = "\t")
+    labels = list(reader)
+np.save('tiny-imagenet-train-labels.npy', labels)
