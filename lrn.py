0b5bde2859a0ba2a71335b11b78de0cd75dbef3e
jianshen205 <jianshen205@gmail.com>
2019-04-21 18:00:21 -0500

    update

--- lrn.py
+++ lrn.py
@@ -3,10 +3,10 @@ import numpy as np
 
 def lrn(layer, radius, bias, alpha, beta):
     #based on paper, fix following values
-    radius = 5.0
-    bias = 2.0
-    alpha = 1e-4
-    beta = 0.75
+#    radius = 5.0
+#    bias = 2.0
+#    alpha = 1e-4
+#    beta = 0.75
     sqr_sum = np.zeros(layer.shape)
     res = np.zeros(layer.shape)
     for x in range(len(layer)):
@@ -24,3 +24,8 @@ def lrn(layer, radius, bias, alpha, beta):
                     sqr_sum[x, y, i] += layer[x, y, j] ** 2
                 res[x, y , i] = float(layer[x, y, i]) /( bias + (alpha * sqr_sum[x, y, i])** beta)
     return res
+
+if __name__ == "__main__":
+    a = np.ones([3,3,3])
+    c = lrn(a, 5.0, 2.0, 1e-4, 0.75)
+    print c
